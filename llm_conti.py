3fbe1d9d04240f11c905ab6f745b92de10c0ac0e
Miguel Ângelo Rebelo <miguelrebelo@protonmail.com>
2023-11-02 23:29:32 +0000

    Add first draft. llm pipeline demo using gradio.

---
+++ llm_conti.py
@@ -0,0 +1,38 @@
+import time
+import gradio as gr
+import torch
+
+from transformers import pipeline
+
+def ask_model(prompt, model, max_tokens=100):
+   
+    start_time = time.time()
+    
+    generate_text = pipeline(
+        model=model, 
+        torch_dtype=torch.bfloat16, 
+        trust_remote_code=True, 
+        device_map="auto"
+    )
+    
+    result = generate_text(
+        prompt,
+        max_new_tokens=max_tokens
+    )
+
+    answer = result[0]["generated_text"]
+
+    elapsed_time = time.time() - start_time
+
+    return answer, elapsed_time
+
+if __name__ == "__main__":
+
+    available_models = ["databricks/dolly-v2-7b"]
+    print(available_models)
+
+    demo = gr.Interface(fn=ask_model, 
+                        inputs=["text", gr.Dropdown(available_models, label="model", value="databricks/dolly-v2-7b")], 
+                        outputs=["text", "number"],
+                        title="ContiGPT")
+    demo.launch(share=True)
